2e543e37a2423dc6239b29f55eedff26ade51f42
xycjscs <yuchao-xu@hotmail.com>
2024-05-14 12:51:07 +0800

    change payment

--- client/src/components/Nav/TokenModal.jsx
+++ client/src/components/Nav/TokenModal.jsx
@@ -42,7 +42,7 @@ function TokenModal({ open, onClose }) {
                   务必备注邮箱，核对收款人（**超）
                 </p>
                 <img
-                  src="/assets/token-image-min.jpg"
+                  src="/assets/token-image.jpg"
                   alt="Token Image"
                   style={{ width: '200px', display: 'block', margin: 'auto' }}
                 />
