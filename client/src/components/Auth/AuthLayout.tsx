a411b88865ab67826e1a7b60dceb5d737f5c9588
xycjscs <yuchao-xu@hotmail.com>
2024-12-22 12:04:10 +0800

    update logo size

--- client/src/components/Auth/AuthLayout.tsx
+++ client/src/components/Auth/AuthLayout.tsx
@@ -61,7 +61,7 @@ function AuthLayout({
     <div className="relative flex min-h-screen flex-col bg-white dark:bg-gray-900">
       <Banner />
       <BlinkAnimation active={isFetching}>
-        <div className="mt-6 h-10 w-full bg-cover">
+        <div className="mt-6 h-40 w-full bg-cover">
           <img
             src="/assets/logo.png"
             className="h-full w-full object-contain"
